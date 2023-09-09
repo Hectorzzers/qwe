4383522dce23811aa9844022c3ae6fb5f8f0b349
Ediz Walker <81444341+Hectorzzers@users.noreply.github.com>
2023-09-09 17:47:56 +0300

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+Valorant offsets 7.05
+
+    ProcessEvent = 0x3c7c520,
+		StaticFindObject = 0x3c90bc0,
+		SkinChanger = 0x2BD47C0,
+		SetAresOutlineMode = 0x56d95e0,
+		bone_matrix = 0x572a5f0,
+		GetActorBounds = 0x54fd0a0,
+ 
+		GetSpreadValues = 0x2b9dd70,
+		GetSpreadAngles = 0x32ee460,
+		GetFiringLocationAndDirection = 0x30f6060,
+		ToVectorAndNormalize = 0x398c430,
+		ToAngleAndNormalize = 0x3988c20,
+ 
+		FiringStateComponent = 0x1020,
+		SeedData = 0x480,
+		StabilityComponent = 0x470,
+ 
+		ErrorPower = 0x450,
+		ErrorRetries = ErrorPower + 0x4,
+ 
+		OwningGameInstance = 0x1A0,
+ 
+		portrait_map = 0x1198,
+		character_map = portrait_map + 0x8;
---
+++ README.md
@@ -0,0 +1,2 @@
+# Latest valorant offsets for 7.05
+Valorant offsets
